e7c7eb070523b07c5785677830fbc791f473b3a3
Phaneendra488 <84266142+Phaneendra488@users.noreply.github.com>
2021-06-14 19:20:04 +0530

    Add files via upload

---
+++ number3 (1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=0
+b=1
+count=1
+x=0
+n=int(input("please enter the limit:"))
+while count<=n:
+    count+=1
+    print(x)
+    x=a+b
+    
+    a=b
+    b=x
+    
+
+
+# In[ ]:
+
+
+
+
