092ef6ffda31307b0187359d2cfdafe350241e05
Phaneendra488 <84266142+Phaneendra488@users.noreply.github.com>
2021-06-14 19:28:26 +0530

    Add files via upload

---
+++ number3 (2).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+my_list=[1,-2,3,67,-98]
+for x in my_list:
+    if x>0:
+        print(x)
+    else:
+        continue
+    
+
+
+# In[ ]:
+
+
+
+
